694cf6dd2ea6d2ba96eb8f2716635baa4bcc264b
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-11 16:55:17 -0300

    Objeto pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,5 +45,15 @@ const sapatos = {
             preco:preco,
             img:img
         })
+    },
+    alterar(nome,desc,categoria,preco,img){
+        for ( let i=0; i < sapatos.listaSapatos.length; i++ ){
+            if (sapatos.listaSapatos[i].nome == nome){
+                sapatos.listaSapatos[i].desc = desc;
+                sapatos.listaSapatos[i].categoria = categoria;
+                sapatos.listaSapatos[i].preco = preco
+                sapatos.listaSapatos[i].img = img
+            }
+        }
     }
 }
